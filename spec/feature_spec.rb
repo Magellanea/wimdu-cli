5edc56e507cd8a4afdf2f6ab24ac611125d53b74
Magellnea <magellanea@gmail.com>
2015-05-30 18:15:58 +0300

    Adding additional test cases. * Adding test case for perfect scenario. * Adding test case for email validation.

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -26,6 +26,34 @@ describe "Wimdu CLI" do
       expect(process.output).to include("Error")
     end
 
+    it "validates emails" do
+      process = run_interactive(cmd)
+      expect(process.output).to include("Starting with new property")
+      expect(process.output).to include("Title: ")
+      type "My Title"
+      expect(process.output).to include("Address: ")
+      type "My Address"
+      expect(process.output).to include("Nightly Rate EUR: ")
+      type "2"
+      expect(process.output).to include("Max guests")
+      type "2"
+      expect(process.output).to include("Email")
+      type "invalid@com"
+      expect(process.output).to include("Error")
+    end
+
+    it "saves an offer when everything is fine" do
+      process = run_interactive(cmd)
+      type "My title"
+      type "My address"
+      type "2"
+      type "2"
+      type "user@wimdu.com"
+      type "123015"
+      expect(process.output).to include("Great job!")
+    end
+
+
     # Please extend!
   end
 end
